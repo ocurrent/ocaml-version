54ee07cd80b47de1a0f897bd67c94732ebf1f5b0
Anil Madhavapeddy <anil@recoil.org>
2018-04-27 21:18:11 +0200

    add 4.08 as a development version

--- ocaml_version.ml
+++ ocaml_version.ml
@@ -99,19 +99,22 @@ module Releases = struct
   let v4_07_0 = of_string_exn "4.07.0"
   let v4_07 = v4_07_0
 
+  let v4_08_0 = of_string_exn "4.08.0"
+  let v4_08 = v4_08_0
+
   let all_patches = [
     v4_00_1; v4_01_0; v4_02_0; v4_02_1; v4_02_2;
     v4_02_3; v4_03_0; v4_04_0; v4_04_1; v4_04_2;
-    v4_05_0; v4_06_0; v4_06_1; v4_07_0 ]
+    v4_05_0; v4_06_0; v4_06_1; v4_07_0; v4_08_0 ]
 
   let all = [ v4_00; v4_01; v4_02; v4_03;
-    v4_04; v4_05; v4_06; v4_07 ]
+    v4_04; v4_05; v4_06; v4_07; v4_08_0 ]
 
-  let recent = [ v4_02; v4_03; v4_04; v4_05; v4_06; v4_07 ]
+  let recent = [ v4_03; v4_04; v4_05; v4_06; v4_07 ]
 
   let latest = v4_06
 
-  let dev = [ ]
+  let dev = [ v4_08_0 ]
 
   let recent_with_dev = List.concat [recent;dev]
 
