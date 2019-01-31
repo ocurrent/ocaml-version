7de01eaa65485242325ae4d31c83b3cb8a4fcf84
Anil Madhavapeddy <anil@recoil.org>
2019-01-31 14:30:18 +0000

    corrections to the trunk variants available

--- ocaml_version.ml
+++ ocaml_version.ml
@@ -209,9 +209,9 @@ end
 
 let compiler_variants arch {major; minor; _} =
     match major,minor,arch with
-    | 4,9,`X86_64 -> [[]; [`Afl]; [`Flambda]; [`Flambda;`Frame_pointer]; [`Default_unsafe_string]; [`Force_safe_string]]
-    | 4,9,_ -> [[]; [`Afl]; [`Flambda];[`Default_unsafe_string]; [`Force_safe_string]]
-    | 4,8,`X86_64 -> [[]; [`Afl]; [`Flambda]; [`Flambda;`Frame_pointer]; [`Default_unsafe_string]; [`Force_safe_string]]
+    | 4,9,`X86_64 -> [[]]
+    | 4,9,_ -> [[]]
+    | 4,8,`X86_64 -> [[]; [`Afl]; [`Flambda]; [`Frame_pointer]; [`Frame_pointer;`Flambda]; [`Default_unsafe_string]; [`Force_safe_string]]
     | 4,8,_ -> [[]; [`Afl]; [`Flambda];[`Default_unsafe_string]; [`Force_safe_string]]
     | 4,7,_ -> [[]; [`Afl]; [`Flambda]; [`Default_unsafe_string]; [`Force_safe_string]]
     | 4,6,_ -> [[]; [`Afl]; [`Flambda]; [`Default_unsafe_string]; [`Force_safe_string]]
