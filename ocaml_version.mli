31ff60176e032f643ab00738e7c6123fdb82eba2
Kate <kit.ty.kate@disroot.org>
2021-03-02 16:21:43 +0000

    Add support for the nnpchecker option

--- ocaml_version.mli
+++ ocaml_version.mli
@@ -393,7 +393,8 @@ module Configure_options : sig
     | `Flambda
     | `Force_safe_string
     | `Frame_pointer
-    | `No_naked_pointers ]
+    | `No_naked_pointers
+    | `No_naked_pointers_checker ]
 
   (** Configuration options available at compiler build time. *)
 
